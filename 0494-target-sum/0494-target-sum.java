0c17c0c6497c1f46c30da6721e2c76c6b070e5b7
Pradeep Kumar <165546461+futSoftDev@users.noreply.github.com>
2025-11-30 17:14:20 +0530

    Time: 8 ms (61.95%), Space: 44.9 MB (39.75%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -6,14 +6,14 @@ class Solution {
         if((total+target)%2!=0  || Math.abs(target)>total) return 0;
         int k=(total+target)/2;
         int dp[][]=new int [n+1][k+1];
-        for(int i=0; i<=n; i++){
-            dp[i][0]=1;
-        }
+        dp[0][0]=1;
         for(int i=1; i<=n; i++){
             for(int j=0; j<=k; j++){
-                 dp[i][j]=dp[i-1][j];
+                 int skip=dp[i-1][j];
+                 int take=0;
                 if(nums[i-1]<=j)
-                dp[i][j]+=dp[i-1][j-nums[i-1]];
+                 take+=dp[i-1][j-nums[i-1]];
+                dp[i][j]=take+skip;
             }
         }
         return dp[n][k];
